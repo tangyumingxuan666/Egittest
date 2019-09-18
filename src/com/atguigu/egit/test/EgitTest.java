1351e14a73608886ee826d04787278d768e5f9e3
tangyu <1355578291@qq.com>
2019-09-18 16:28:22 +0800

    克隆添加1

--- src/com/atguigu/egit/test/EgitTest.java
+++ src/com/atguigu/egit/test/EgitTest.java
@@ -4,5 +4,9 @@ public class EgitTest {
 	
 	public static void main(String[] args) {
 		System.out.println("aaa");
+		System.out.println("bbb");
+		System.out.println("ccc");
+		System.out.println("ddd");
+		System.out.println("10086");
 	}
 }
